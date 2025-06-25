ed7226158dbb66b8d9f107800ffa60272f264bf4
iremturen <iremturennn@gmail.com>
2025-06-25 19:15:03 +0300

    19.14

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ituren <ituren@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 17:12:28 by ituren            #+#    #+#             */
-/*   Updated: 2025/06/25 17:50:53 by ituren           ###   ########.fr       */
+/*   Updated: 2025/06/25 19:13:50 by ituren           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
-	i = (int)ft_strlen(s);
+	i = (int)ft_strlen(s) - 1;
 	while (i >= 0)
 	{
 		if (s[i] == (char)c)
